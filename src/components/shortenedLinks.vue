b588e12d60a11d0ba41d45e20bfda9d950e485da
reynoldadade <reynoldadade@gmail.com>
2021-09-10 08:15:10 +0000

    shortenedLink component to hold links from api

---
+++ src/components/shortenedLinks.vue
@@ -0,0 +1,27 @@
+<template>
+	<div
+		class="w-full p-2 bg-white rounded-lg flex justify-between shadow-lg font-semibold"
+	>
+		<div class="p-1">
+			{{ link.original_link }}
+		</div>
+		<div class="text-mcyan p-1">
+			{{ link.full_short_link }}
+		</div>
+		<div>
+			<button class="bg-mcyan text-white py-1 px-4 rounded-lg p-1">
+				copy
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		link: Object,
+	},
+};
+</script>
+
+<style></style>
--- src/components/shorterner.vue
+++ src/components/shorterner.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="flex justify-center w-full">
+	<div class="flex flex-col justify-center items-center w-full">
 		<div class="w-4/6 rounded-lg bg-mviolet p-8">
-			<form @submit.prevent="">
+			<form @submit.prevent="shortenURL(url)">
 				<div class="flex">
 					<input
 						type="url"
@@ -10,8 +10,10 @@
 						placeholder="Shorten a link here.."
 						class=" p-2 flex-grow rounded-lg"
 						required
+						v-model="url"
 					/>
 					<button
+						type="submit"
 						class="bg-mcyan text-white px-6 py-2 mx-4 rounded-lg"
 					>
 						Shorten it!
@@ -19,17 +21,34 @@
 				</div>
 			</form>
 		</div>
+		<div class="w-4/6 my-2">
+			<shortened-links
+				v-for="(link, index) in links"
+				:key="index"
+				:link="link"
+			/>
+		</div>
 	</div>
 </template>
 
 <script>
 import axios from "axios";
+import shortenedLinks from "./shortenedLinks.vue";
 export default {
+	components: {
+		shortenedLinks,
+	},
+	data() {
+		return {
+			url: "",
+			links: [],
+		};
+	},
 	name: "shortener",
 	methods: {
 		async GET_shorten(url) {
 			try {
-				const response = await axios.$get(
+				const response = await axios.get(
 					"https://api.shrtco.de/v2/shorten",
 					{
 						params: {
@@ -38,14 +57,17 @@ export default {
 					}
 				);
 				return response.data;
-			} catch ({ response }) {}
+			} catch ({ response }) {
+				console.error(response);
+			}
 		},
-		shortenURL(url) {
-            const response = await this.GET_shorten(url);
-            if(response){
-                console.log(response)
-            }
-        },
+		async shortenURL(url) {
+			const response = await this.GET_shorten(url);
+			if (response) {
+				console.log(response);
+				this.links = [...this.links, response.result];
+			}
+		},
 	},
 };
 </script>
