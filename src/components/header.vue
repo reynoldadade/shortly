6a961e15bc58c9afd075a4922f92743e76d629e1
reynoldadade <reynoldadade@gmail.com>
2021-09-10 07:14:44 +0000

    added nuxt js

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,10 @@
 <template>
-	<div class="w-full p-2 flex">
-		<img src="/src/assets/images/logo.svg" alt="" />
+	<div class="w-full p-2 flex justify-between">
+		<img src="/src/assets/images/logo.svg" alt="Shortly" />
+		<div>
+			<button>Login</button>
+			<button>Sign up</button>
+		</div>
 	</div>
 </template>
 
