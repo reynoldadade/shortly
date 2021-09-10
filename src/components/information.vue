382ef64ade28a81c008c200567f6d198522e89ee
reynoldadade <reynoldadade@gmail.com>
2021-09-10 08:25:58 +0000

    infoCard component to hold our feature info

---
+++ src/components/information.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="w-full">
+		<div>
+			Advanced Statistics
+		</div>
+		<div>
+			Track how your links performing actions across the internet with our
+			advanced statistics dahsboard
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/components/shortenedLinks.vue
+++ src/components/shortenedLinks.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="w-full p-2 bg-white rounded-lg flex justify-between shadow-lg font-semibold"
+		class="w-full p-2 bg-white rounded-lg flex justify-between shadow-lg font-semibold my-2"
 	>
 		<div class="p-1">
 			{{ link.original_link }}
